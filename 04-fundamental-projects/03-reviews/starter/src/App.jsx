2536a640c1837fc1754d0f295e6374df24a4b063
Mujaahid Ajam <Mujaahid.Ajam@redpandasoftware.co.za>
2023-11-01 16:16:59 +0200

    Last change was the random number function

--- 04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,65 @@
+import { useState } from "react";
+import poeple from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = poeple[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex)=>{
+      const newIndex = currentIndex + 1;
+      if(newIndex > poeple.length -1) {
+        return 0;
+      }
+      return newIndex;
+  });
+};
+
+  const prevPerson = () => {
+    setIndex((currentIndex)=>{
+      const newIndex = currentIndex - 1;
+      if(newIndex < 0) {
+        return poeple.length - 1;
+        
+      }
+      return newIndex;
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * poeple.length)
+    if(randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNum(currentIndex)
+  };
+
+
+  return (
+    <main>
+      <article className="review">
+        <div className="ima-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight/>
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft/>
+          </button>
+          <button className="btn btn-hipster" onClick={randomPerson}>Random Person</button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight/>
+          </button>
+        </div>
+        
+      </article>
+    </main>
+  )
 };
 export default App;
